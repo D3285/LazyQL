ebecbd6b46ee461f0e262850eff14cad64c9a7d8
Aditya Aryan <erenyeager200026@gmail.com>
2026-08-27 23:36:10 +0530

    fix: configure production CORS

--- server/app/main.py
+++ server/app/main.py
@@ -13,13 +13,13 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://YOUR-FRONTEND.vercel.app",
+        "https://lazy-ql-git-main-aditya-aryan-26-projects1.vercel.app",
+        "https://lazy-ql.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(database_router)
 app.include_router(generate_router)
